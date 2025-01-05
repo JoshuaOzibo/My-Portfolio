710ee35cd1c0e67c337a9cbb4a7ce8d2cca3957e
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-01-05 08:04:15 +0100

    Add SatoshiFont to index.css and update Navbar component to use the new font for improved typography

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,12 @@ const Navbar = () => {
     <div className="w-full z-20 fixed flex h-16 justify-center items-center bg-white/40 backdrop-blur-lg">
       <div className="w-[85%] h-full items-center flex md:justify-between">
         {/* Header */}
-        <h1 className="md:text-xl font-mono md:block hidden sm:text-sm text-xs font-black uppercase">
+        <h1 className="md:text-xl font-[SatoshiFont] md:block hidden sm:text-sm  font-black text-xs uppercase">
           Portfolio
         </h1>
 
         {/* Logos and Menu */}
-        <div className=" md:flex hidden justify-between w-[40%] rounded-full px-16 py-2 border border-black">
+        <div className=" md:flex hidden justify-between w-[40%] rounded-full px-16 py-2">
           <a
             href="#"
             onClick={(e) => {
@@ -29,17 +29,17 @@ const Navbar = () => {
               window.scrollTo({ top: 0 });
             }}
           >
-            <h1 className="text-sm cursor-pointer font-serif font-semibold">
+            <h1 className="text-sm font-[SatoshiFont] cursor-pointer font-semibold">
               Home
             </h1>
           </a>
           <a href="#projects">
-            <h1 className="text-sm cursor-pointer font-serif font-semibold">
+            <h1 className="text-sm font-[SatoshiFont] cursor-pointer font-semibold">
               Project
             </h1>
           </a>
           <a href="#skills">
-            <h1 className="text-sm cursor-pointer font-serif font-semibold">
+            <h1 className="text-sm font-[SatoshiFont] cursor-pointer font-semibold">
               Skill
             </h1>
           </a>
